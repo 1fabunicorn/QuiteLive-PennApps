e6c150fb0abe97bdf7275f241f3370215df91515
Nova <noah.trauben@gmail.com>
2019-09-07 15:22:15 -0400

    Switch to Flask :(

--- routes/verify.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {PythonShell} = require("python-shell");
-const {exec} = require('child_process');
-const exphbs = require('express-handlebars');
-
