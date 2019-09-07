c2635fdc48dde0c07af8fe3f767d5cb46d19905b
Wyatt Phillips <datscigye@gmail.com>
2019-09-07 18:43:44 -0400

    Fix routes and reference links

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,8 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Home')
+    return render_template('index.html', title='QuiteLive Dashboard')
+
+@app.route('/info')
+def info():
+    return render_template('info.html', title='About QuiteLive')
