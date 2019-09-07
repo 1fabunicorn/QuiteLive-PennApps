751fbac18ba4fa37192ab8f37911997f81ad50fa
Chase Rensberger <chr5119@psu.edu>
2019-09-07 01:00:27 -0400

    Create list of objects with the matrix data of each frame

--- VideoToFrames.py
+++ VideoToFrames.py
@@ -1,8 +1,13 @@
 # Program To Read video 
 # and Extract Frames 
-import cv2 
+import cv2
+from src import Frame
+from PIL import Image
 
-  
+#List of objects that holds the data of each frame
+FrameList = []
+
+
 # Function to extract frames 
 def FrameCapture(path): 
       
@@ -22,14 +27,18 @@ def FrameCapture(path):
         success, image = vidObj.read() 
   
         # Saves the frames with frame-count 
-        cv2.imwrite("frame%d.jpg" % count, image) 
-  
+        #cv2.imwrite("frame%d.jpg" % count, image) 
+
+        #Creates an object at 
+        FrameList.append(Frame.Frame(image))
+
         count += 1
   
+
 # Driver Code 
 if __name__ == '__main__': 
   
     # Calling the function 
     FrameCapture(r"C:\Users\chase\Desktop\TestVideoDataCapture\Sample.mp4") 
 
-
+    
---
+++ src/Frame.py
@@ -0,0 +1,7 @@
+
+
+class Frame:
+    
+    def __init__(self, FrameData):
+        
+        self.FrameData = FrameData
