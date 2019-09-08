ecb5f057818a2a27e1fff3e9fe6b603a2ba3ceaa
Wyatt Phillips <phillipsw1@wit.edu>
2019-09-08 05:18:42 -0400

    Auto stash before revert of "Optimize video processing"

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, session
 from app import app
 from app import socketio
+import eventlet
 
 from PIL import Image
 from io import BytesIO
@@ -12,8 +13,11 @@ import numpy
 import os
 import shutil
 
+from app import sendOp
+from app import hashFile
+
 stream_path = './stream/'
-global image_count
+
 
 def pil_image_to_base64(pil_image):
     buf = BytesIO()
@@ -38,7 +42,9 @@ def event(message):
 
 @socketio.on('frame')
 def event(frame):
+
     frame = frame.split(",")[1]
+<<<<<<< Updated upstream
     img = base64_to_pil_image(frame)
 
     session['vidout'].write(numpy.array(img)[:, :, ::-1].copy())
@@ -46,8 +52,108 @@ def event(frame):
 @socketio.on('stream-start')
 def streamStart(packet):
     session['vidout'] = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc(*'XVID'), 24.0, (640,480))
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+    image = base64_to_pil_image(frame)
+    image.save(stream_path + str(session['count']) + ".jpg")
+    if os.path.exists(stream_path + str(session['count']) + ".jpg"):
+        session['count'] = session['count'] + 1
+<<<<<<< Updated upstream
+
+@socketio.on('stream-start')
+def streamStart(packet):
+    print("STREAM STARTING")
+=======
+
+@socketio.on('stream-start')
+def streamStart(packet):
+    print("STREAM STARTING")
+=======
+<<<<<<< HEAD
+    img = base64_to_pil_image(frame)
+
+    session['vidout'].write(numpy.array(img)[:, :, ::-1].copy())
+
+@socketio.on('stream-start')
+def streamStart(packet):
+    session['vidout'] = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc(*'XVID'), 24.0, (640,480))
+=======
+    image = base64_to_pil_image(frame)
+    image.save(stream_path + str(session['count']) + ".jpg")
+    if os.path.exists(stream_path + str(session['count']) + ".jpg"):
+        session['count'] = session['count'] + 1
+    if session['startFlag']:
+        hashOfFirstFrame = hashFile.sha256sum(stream_path + str(session['count'] - 1) + ".jpg")
+        session['FirstFrameHash'] = sendOp.sendOpTx(hashOfFirstFrame)
+
+
+@socketio.on('stream-start')
+def streamStart(packet):
+    print("STREAM STARTING")
+    session['startFlag'] = True
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+    try:
+        if os.path.exists(stream_path):
+            shutil.rmtree(stream_path)
+        os.mkdir(stream_path)
+    except OSError:
+        print(OSError)
+
+    session['count'] = 0
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+>>>>>>> nova
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 @socketio.on('stream-end')
 def streamEnd(packet):
     print("STREAM ENDING")
+<<<<<<< Updated upstream
     session['vidout'].release()
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+    img1 = cv2.imread(stream_path + '0.jpg')
+
+    height, width, layers = img1.shape
+    vidout = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc(*'XVID'), 24.0, (640,480))
+<<<<<<< Updated upstream
+=======
+=======
+<<<<<<< HEAD
+    session['vidout'].release()
+=======
+    img1 = cv2.imread(stream_path + '0.jpg')
+
+    vidout = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc(*'XVID'), 24.0, (640, 480))
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+    vidout.write(img1)
+    for i in range(1, session['count']):
+        #print(stream_path + str(i) + '.jpg')
+        framed = cv2.imread(stream_path + str(i) + '.jpg')
+        vidout.write(framed)
+
+<<<<<<< Updated upstream
+    cv2.destroyAllWindows()
+    vidout.release()
+=======
+<<<<<<< Updated upstream
+    cv2.destroyAllWindows()
+    vidout.release()
+=======
+    vidout.release()
+>>>>>>> nova
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
